d1c7d6df6058096ddb2a2f401f6196ac656859d1
dobraflorin <dobra.florin@gmail.com>
2023-05-25 19:47:53 +0300

    Initial commit

---
+++ src/main/java/com/pcrepairservice/pcservice/PcserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pcrepairservice.pcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcrepairservice/pcservice/PcserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcrepairservice.pcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
