a8e3029408574d0c7cd5779a85f66e9dd897150a
dobraflorin <dobra.florin@gmail.com>
2023-05-25 19:50:10 +0300

    update1

--- src/main/java/com/pcrepairservice/pcservice/ComputerServiceApplication.java
+++ src/main/java/com/pcrepairservice/pcservice/ComputerServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PcserviceApplication {
+public class ComputerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PcserviceApplication.class, args);
+		SpringApplication.run(ComputerServiceApplication.class, args);
 	}
 
 }
