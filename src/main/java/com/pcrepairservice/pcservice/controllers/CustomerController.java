ab2f7c969596809b40f0227e9c432d3b23722919
dobraflorin <dobra.florin@gmail.com>
2023-05-29 18:51:58 +0300

    update3

--- src/main/java/com/pcrepairservice/pcservice/controllers/CustomerController.java
+++ src/main/java/com/pcrepairservice/pcservice/controllers/CustomerController.java
@@ -1,4 +1,17 @@
 package com.pcrepairservice.pcservice.controllers;
 
+import com.pcrepairservice.pcservice.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
 }
+
