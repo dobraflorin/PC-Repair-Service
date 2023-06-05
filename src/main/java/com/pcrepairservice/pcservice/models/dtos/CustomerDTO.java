89c7d069aea1e1db2531ca9a28e8d786a041070e
dobraflorin <dobra.florin@gmail.com>
2023-06-05 18:58:30 +0300

    update ProductDTO

--- src/main/java/com/pcrepairservice/pcservice/models/dtos/CustomerDTO.java
+++ src/main/java/com/pcrepairservice/pcservice/models/dtos/CustomerDTO.java
@@ -1,4 +1,11 @@
 package com.pcrepairservice.pcservice.models.dtos;
 
+import lombok.Data;
+
+@Data
 public class CustomerDTO {
+    private Long id;
+    private String fistName;
+    private String lastName;
+    private String email;
 }
--- src/main/java/com/pcrepairservice/pcservice/models/dtos/ProductDTO.java
+++ src/main/java/com/pcrepairservice/pcservice/models/dtos/ProductDTO.java
@@ -1,4 +1,14 @@
 package com.pcrepairservice.pcservice.models.dtos;
 
-public class ProductDTO {
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class ProductDTO implements Serializable {
+    private long id;
+    private String model;
+    private String price;
+    private String problem;
+    private String solution;
+
 }
