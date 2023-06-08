5c063fd0a0241f014461ffa167d3fb665e0883df
dobraflorin <dobra.florin@gmail.com>
2023-06-08 19:22:03 +0300

    update classes

--- src/main/java/com/pcrepairservice/pcservice/models/entities/Product.java
+++ src/main/java/com/pcrepairservice/pcservice/models/entities/Product.java
@@ -1,8 +1,15 @@
 package com.pcrepairservice.pcservice.models.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Set;
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name = "products")
 @Entity
 @Data
@@ -11,4 +18,14 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "price")
+    private Double price;
+    @Column(name = "problem")
+    private String problem;
+    @Column(name = "solution")
+    private String solution;
+    @ManyToMany(mappedBy = "products")
+    private Set<Customer> customers;
 }
